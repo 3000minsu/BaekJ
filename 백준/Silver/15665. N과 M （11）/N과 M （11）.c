9f874c46a5281afd3718ddaf5792d5a3bfb9b468
3000minsu <64120287+3000minsu@users.noreply.github.com>
2022-06-16 22:20:24 +0900

    [Silver II] Title: N과 M (11), Time: 168 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int n, m, rn=0;
+int arr[7] = { 0 };
+int ans[7];
+void dfs(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            printf("%d ", ans[i]);
+        printf("\n");
+        return;
+    }
+    for (int i = 0; i < rn; i++)
+    {
+            ans[cnt] = arr[i];
+            dfs(cnt + 1);
+    }
+}
+int main()
+{
+    int temp,temp2, a, ch=0, i=0;
+    scanf("%d %d", &n, &m);
+    while(i<n)
+    {
+        scanf("%d", &a);
+        for (int j = 0; j < rn; j++)
+        {
+            if (arr[j] == a)
+                ch = 1;
+        }
+        if (ch == 0)
+        {
+            arr[rn] = a;
+            rn++;
+        }
+        i++;
+        ch = 0;
+    }
+    for (int i = 0; i < rn; i++)//오름차순 정렬
+    {
+        for (int j = 0; j <rn - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    dfs(0);
+    return 0;
+}
