653be7ecd4901670faf6a2e96cca43636d760399
3000minsu <64120287+3000minsu@users.noreply.github.com>
2022-06-18 14:14:03 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int arr1[50];
+    int arr2[50];
+    int n, temp;
+    scanf("%d", &n);
+    for(int i=0;i<n; i++)
+        scanf("%d", &arr1[i]);
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr2[i]);
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-1; j++)
+        {
+            if(arr1[j]>arr1[j+1])
+            {
+                temp=arr1[j];
+                arr1[j]=arr1[j+1];
+                arr1[j+1]=temp;
+            }  
+        }
+    } 
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-1; j++)
+        {
+            if(arr2[j]<arr2[j+1])
+            {
+                temp=arr2[j];
+                arr2[j]=arr2[j+1];
+                arr2[j+1]=temp;
+            }  
+        }
+    }
+    temp=0;
+    for(int i=0; i<n; i++)
+        temp+=arr1[i]*arr2[i];
+    printf("%d", temp);
+    return 0;
+}
