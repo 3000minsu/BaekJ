afdc3e4be9ae3c374aae3e2c443ad8556a6dc321
3000minsu <64120287+3000minsu@users.noreply.github.com>
2022-06-28 16:58:24 +0900

    [Silver IV] Title: 로프, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -3,15 +3,16 @@
 using namespace std;
 int main()
 {
+    //최대무게= 최소 무게 * 로프 갯수
     priority_queue < int> pq;
     int n, a, max=0;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> a;
-        pq.push(-a);
+        pq.push(-a);//작은거 먼저
     }
-    while (!pq.empty())
+    while (!pq.empty())//가장 약한 로프 하나씩 제거하면서
     {
         int temp;
         temp = -pq.top();
