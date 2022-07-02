30aada6cf6810d22c97d316bceb2f6847bd5aeae
3000minsu <64120287+3000minsu@users.noreply.github.com>
2022-07-02 16:12:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 396 ms, Memory: 8180 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -2,19 +2,16 @@
 using namespace std;
 int main()
 {
-    priority_queue<int> pq;
     int n, temp;
+    vector<int> v;
     scanf("%d", &n);
     for(int i=0; i<n; i++)
     {
         scanf("%d", &temp);
-        pq.push(-temp);
+        v.push_back(temp);
     }
-    while(!pq.empty())
-    {
-        temp=-pq.top();
-        pq.pop();
-        cout<<temp<<"\n";
-    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<n; i++)
+        cout<<v[i]<<"\n";
     return 0;
 }
