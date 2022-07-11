e484270b90b19c710ea02c68cc2c171015d1b0bc
3000minsu <64120287+3000minsu@users.noreply.github.com>
2022-07-11 23:34:06 +0900

    [Silver II] Title: 스택 수열, Time: 1712 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+bool vi[100001] = { false };
+bool ans[300001] = { false };
+stack<int> s;
+int main()
+{
+    int temp,c=0,cnt=0;
+    scanf("%d", &n);
+    for (int j = 0; j < n; j++)
+    {
+        scanf("%d", &temp);
+        if (s.empty())
+        {
+            for (int i = 1; i <= temp; i++)
+            {
+                if (!vi[i])
+                {
+                    s.push(i);
+                    vi[i] = true;
+                    ans[cnt] = true;
+                    cnt++;
+                }
+            }
+            s.pop();
+            ans[cnt] = false;
+            cnt++;
+        }
+        else if (s.top() > temp)
+        {
+            c = 1;
+            break;
+        }
+        else if (s.top() == temp)
+        {
+            s.pop();
+            ans[cnt] = false;
+            cnt++;
+        }
+        else if (s.top() < temp)
+        {
+            for (int i = s.top(); i <= temp; i++)
+            {
+                if (!vi[i])
+                {
+                    s.push(i);
+                    vi[i] = true;
+                    ans[cnt] = true;
+                    cnt++;
+                }
+            }
+            s.pop();
+            ans[cnt] = false;
+            cnt++;
+        }
+    }
+    if (c == 1)
+        printf("NO");
+    else
+    {
+        for (int i = 0; i < cnt; i++)
+        {
+            if (ans[i])
+                printf("+\n");
+            else
+                printf("-\n");
+        }
+
+    }
+
+
+
+
+    return 0;
+}
